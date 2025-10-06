f01d36c4c33b7777b62d58b6928b3eaaa1949e96
yyxxryrx <yyxxryrx@aliyun.com>
2025-10-06 16:19:23 +0800

    feat(config): 添加线程数配置项
    - 新增 ThreadCount 属性用于获取和设置线程数量
    - 优化 Get 方法的类型转换逻辑
    - 保持与现有 Language 配置项一致的实现方式

--- Modules/Services/ConfigurationService.cs
+++ Modules/Services/ConfigurationService.cs
@@ -5,12 +5,18 @@ namespace Downloader.Modules.Services;
 public class ConfigurationService
 {
     private ApplicationDataContainer Configuration { get; } = ApplicationData.Current.LocalSettings;
-    private T Get<T>(string key, T defaultValue) where T : class => Configuration.Values[key] as T ?? defaultValue;
-    private void Set<T>(string key, T value) where T : class => Configuration.Values[key] = value;
+    private T Get<T>(string key, T defaultValue) => Configuration.Values[key] is T t ? t : defaultValue;
+    private void Set<T>(string key, T value) => Configuration.Values[key] = value;
 
     public string Language
     {
         get => Get("Language", string.Empty);
         set => Set("Language", value);
+    }
+    
+    public int ThreadCount
+    {
+        get => Get("ThreadCount", 8);
+        set => Set("ThreadCount", value);
     }
 }
